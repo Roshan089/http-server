933525304456e55b8bc603278f932c6e9cc8cfe0
Roshan <1js20is089@gmail.com>
2024-03-14 16:47:32 +0530

    moddleware

---
+++ middleware.js
@@ -0,0 +1,28 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+//route handler
+app.get('/', function(req, res) {
+  res.send('Hello World!')
+})
+
+app.post("/health", (req, res) =>{
+    const kidney = req.body.kidny;
+    const kidnylen = kidney.length;
+    
+    res.send(`you have ${kidnylen} lenkidneys`)
+})
+
+app.use(function (err, req, res, next) {
+    res.json({
+        msg: "sorry somthing went wrong"
+    })
+})
+
+app.listen(port, function()  {
+  console.log(`Example app listening on port ${port}`)
+})
