4ef23431eab596a3276fb8b20ad3a941cb9cf3fc
Roshan <1js20is089@gmail.com>
2024-03-16 02:17:00 +0530

    zod

--- middleware.js
+++ middleware.js
@@ -2,8 +2,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const zod = require("zod")
 
 app.use(express.json());
+
+const  schema=zod.array(zod.number())
 
 //route handler
 app.get('/', function(req, res) {
@@ -12,17 +15,31 @@ app.get('/', function(req, res) {
 
 app.post("/health", (req, res) =>{
     const kidney = req.body.kidny;
-    const kidnylen = kidney.length;
     
-    res.send(`you have ${kidnylen} lenkidneys`)
+    const respons = schema.safeParse(kidney)
+    
+    if (!respons.success) {
+        res.status(411).json({
+         mes: "jhfgdyjfj"
+        })
+    }
+    
+    else {
+        res.send(
+            {
+                respons
+            }
+        )
+   }
 })
 //gkobal  catches
+/*
 app.use(function (err, req, res, next) {
     res.json({
         msg: "sorry somthing went wrong cccc"
     })
 })
-
+*/
 app.listen(port, function()  {
   console.log(`Example app listening on port ${port}`)
 })
